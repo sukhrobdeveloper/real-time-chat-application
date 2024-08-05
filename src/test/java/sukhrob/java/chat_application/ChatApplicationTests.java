7716d02d91d8ab9b84a3df9856f88a643fc14d79
sukhrobdeveloper <sukhrobtokhirov006@gmail.com>
2024-08-05 18:02:33 +0500

    project created

---
+++ src/test/java/sukhrob/java/chat_application/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package sukhrob.java.chat_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
